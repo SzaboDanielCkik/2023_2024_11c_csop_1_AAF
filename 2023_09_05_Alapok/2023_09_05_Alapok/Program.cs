252eaa896f58d9b2170ce81b8233c41251ea8abb
Szabó Dániel <szabo.daniel@ckik.hu>
2023-09-06 08:36:14 +0200

    2023_09_05

---
+++ 2023_09_05_Alapok/2023_09_05_Alapok/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2023_09_05_Alapok
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Egy soros komment
+            /*
+             Több soros komment
+             */
+
+            Console.WriteLine("Első sorom a programban. :-)"); 
+            // A paraméterben megadott szöveget, változót, objektumokhoz rendelt információkat írja ki a konzolra
+            // A kiírt szöveg után, új sorban kezd.
+
+            Console.Write("Második sorom a programban. :-)");
+            // A paraméterben megadott szöveget, változót, objektumokhoz rendelt információkat írja ki a konzolra
+            // A kurzor az utolsó karaker után lesz elérhető
+            Console.WriteLine();
+
+
+            Console.ReadLine(); // Enter leütésére vár.
+            //Console.ReadKey(); // Billentyű leütésére vár.
+        }
+    }
+}

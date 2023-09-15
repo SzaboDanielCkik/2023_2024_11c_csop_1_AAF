482cc3f79448e7b01df9ca911119d3421ad4461f
Szabó Dániel <szabo.daniel@ckik.hu>
2023-09-15 10:47:22 +0200

    2023_09_15_ciklusok

--- 2023_09_14_Elagazasok/2023_09_14_Elagazasok/Program.cs
+++ 2023_09_14_Elagazasok/2023_09_14_Elagazasok/Program.cs
@@ -50,6 +50,54 @@ namespace _2023_09_14_Elagazasok
              *         break;
              * }
              */
+
+            // szilárd<0, 0<=folyadék<100, gáz>100
+
+            Random r = new Random();
+            int hofok = r.Next(-100, 201);
+            Console.WriteLine(hofok);
+            if (hofok < 0)
+            {
+                Console.WriteLine("szilárd");
+            }
+            else if (hofok < 100)
+            {
+                Console.WriteLine("folyadék");
+            }
+            else
+            {
+                Console.WriteLine("gáz");
+            }
+
+            //pl. nap - edzésterv
+            string nap = "csütörtok";
+            switch (nap)
+            {
+                case "hétfő": Console.WriteLine("100 guggolás");
+                    break;
+                case "kedd": Console.WriteLine("100 fekvő");
+                    break;
+                case "szerda":
+                    Console.WriteLine("200 felülés");
+                    break;
+                case "csütörtök":
+                    Console.WriteLine("pihi");
+                    break;
+                case "péntek":
+                    Console.WriteLine("100 fekvő 100 felülés");
+                    break;
+                case "szombat":
+                    Console.WriteLine("100 kitörés");
+                    break;
+                case "vasárnap":
+                    Console.WriteLine("pihi");
+                    break;
+                default:
+                    Console.WriteLine("Rosszul írta be a napot!");
+                    break;
+            }
+
+            Console.ReadLine();
         }
     }
 }
---
+++ 2023_09_15_Ciklusok/2023_09_15_Ciklusok/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2023_09_15_Ciklusok
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Számlálós ciklus
+             * ciklus i = kezdőérték - meddig léptetés
+             *      Ciklus Mag
+             * ciklus vége
+             * 
+             * for(int i = 0; i<n; i++) // n-1-ig megy, i+=1 egyesével
+             * { //egy utasítás esetén a zárójel elhagyható
+             *      Ciklus Mag
+             * }
+             * 
+             * Tesztelős ciklusok
+             * Elől tesztelős
+             * i = 0;
+             * Ciklus amíg (belépés feltétele)
+             * {
+             *      Ciklus Mag
+             *      i léptetése;
+             * }
+             * 
+             * int i = 0;
+             * while(i<n && Ttul(random))
+             * {
+             *      random;
+             *      i++;
+             * }
+             * 
+             * Hátul tetsztelős
+             * Ciklus
+             *      Ciklus Mag
+             * Amíg(benmaradás feltétele);
+             * 
+             * do
+             * {
+             *      Ciklus Mag
+             * }while(benmaradás feltétele);
+             */
+        }
+    }
+}

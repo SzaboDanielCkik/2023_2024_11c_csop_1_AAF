b04671ff7556c76e28a5c493a724cf03a17c6193
Szabó Dániel <szabo.daniel@ckik.loc>
2024-02-23 14:04:28 +0100

    2024_02_23

---
+++ 2024_02_22_Sziget10/2024_02_22_Sziget10/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2024_02_22_Sziget10
+{
+    class Program
+    {
+        static List<int> t = new List<int>();
+        static void Main(string[] args)
+        {
+            t= Array.ConvertAll(Console.ReadLine().Split(','), Convert.ToInt32).ToList();
+            int startI = TengerJobbra(0);
+            int endI = TengerBalra(t.Count);
+            int index = MaxTavIndex(startI, endI);
+            Console.WriteLine(2+TengerBalra(index)+ " " + TengerJobbra(index));
+            Console.ReadLine();
+        }
+
+        static int TengerJobbra(int kezdet)
+        {
+            int i = kezdet;
+            while (t[i] != 0) i++;
+            return i;
+        }
+
+        static int TengerBalra(int vege)
+        {
+            int i = vege-1;
+            while (t[i] != 0) i--;
+            return i;
+        }
+
+        static int MaxTavIndex(int startI, int endI)
+        {
+            int maxi = startI;
+            for (int i = startI; i < endI; i++)
+            {
+                if (t[i] != 0 && t[i+1] != 0 &&  Math.Abs(t[i] - t[i + 1]) > Math.Abs(t[maxi] - t[maxi + 1]))
+                    maxi = i;
+            }
+            return maxi;
+        }
+    }
+}
+
+/*
+12
+3 0 1 1 5 4 0 2 5 1 0 2
+*/
---
+++ Megoldas_Tabor/Megoldas_Tabor/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Megoldas_Tabor
+{
+    class Program
+    {
+        static List<Tabor> t = new List<Tabor>();
+        static void Main(string[] args)
+        {
+            Fajlbeolvasas();
+            Feladat2();
+            Feladat3();
+            Feladat4();
+            Console.ReadLine();
+        }
+
+        private static void Feladat4()
+        {
+            List<int> index = MaxJelentkezokIndexe();
+            Console.WriteLine("4. feladat");
+            for (int i = 0; i < index.Count; i++)
+            {
+                Console.WriteLine("Legnépszerűbbek: {0} {1} {2}", t[index[i]].kh, t[index[i]].kn, t[index[i]].tnev);
+            }        
+        }
+
+        private static List<int> MaxJelentkezokIndexe()
+        {
+            List<int> indexek = new List<int>();
+            int maxi = 0;
+            for (int i = 1; i < t.Count; i++)
+            {
+                if (t[i].jelentkezok.Length > t[maxi].jelentkezok.Length)
+                {
+                    indexek.Clear();
+                    maxi = i;
+                    indexek.Add(maxi);
+                }
+                else if (t[i].jelentkezok.Length == t[maxi].jelentkezok.Length)
+                {
+                    indexek.Add(i);
+                }
+            }
+            return indexek;
+        }
+
+        private static int Sorszam(int h, int n)
+        {
+            int napok = 0;
+
+
+            return napok;
+        }
+
+        private static void Feladat3()
+        {
+            Console.WriteLine("3. feladat");
+            List<Tabor> taborok = TaborIdopontok("zenei");
+            for (int i = 0; i < taborok.Count; i++)
+            {
+                Console.WriteLine("Zenei tábor kezdődik {0}.hó {1}.napján.",
+                    taborok[i].kh,taborok[i].kn);
+            }        
+        }
+
+        private static List<Tabor> TaborIdopontok(string tipus)
+        {
+            List<Tabor> taborok = new List<Tabor>();
+            for (int i = 0; i < t.Count; i++)
+            {
+                if (t[i].tnev == tipus)
+                    taborok.Add(t[i]);
+            }
+            return taborok;
+        }
+
+
+        private static void Feladat2()
+        {
+            Console.WriteLine("2. feladat\nAz adatsorok száma: {0}\nAz először rögzített tábor témája: {1}\nAz utoljára rögzített tábor témája: {2}",
+                t.Count,t.First().tnev,t.Last().tnev);
+        }
+
+        private static void Fajlbeolvasas()
+        {
+            StreamReader f = new StreamReader("taborok.txt");
+
+            while (!f.EndOfStream)
+                t.Add(new Tabor(f.ReadLine()));
+
+            f.Close();
+        }
+    }
+
+    struct Tabor
+    {
+        //mezők
+        public int kh, kn, vh, vn;
+        public string jelentkezok, tnev;
+
+        //konstruktor
+        public Tabor(string sor)
+        {
+            string[] st = sor.Split('\t');
+            kh = Convert.ToInt32(st[0]);
+            kn = Convert.ToInt32(st[1]);
+            vh = Convert.ToInt32(st[2]);
+            vn = Convert.ToInt32(st[3]);
+            jelentkezok = st[4];
+            tnev = st[5];
+        }
+    }
+}

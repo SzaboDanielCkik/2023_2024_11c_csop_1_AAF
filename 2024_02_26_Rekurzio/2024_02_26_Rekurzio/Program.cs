0005111cecf7a5adef6488b8ae3bb4ceeb2c065c
Szabó Dániel <szabo.daniel@ckik.loc>
2024-03-07 09:08:24 +0100

    2024_03_07

---
+++ 2024_02_26_Rekurzio/2024_02_26_Rekurzio/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2024_02_26_Rekurzio
+{
+    class Program
+    {
+        static List<int> lista = new List<int>();
+        static Random r = new Random();
+        static void Main(string[] args)
+        {
+            /* Rekurzió - metódus, ami meghívja önmagát.
+               
+               Program(n)
+                    folyamat;
+                    Program(n+1);
+               pr.v.
+            */
+
+            int n = 6;
+            // n! = n * (n-1) * (n-2) * ....* 2 * 1
+            Console.WriteLine(FaktorIterativan(n));
+            // n! = n * (n-1)!
+
+
+            Console.WriteLine(FaktorRekurzivan(n));
+
+            Console.WriteLine(FibonacciIterativan());
+            Console.WriteLine(FibonacciRekurzivan(10));
+            Kiiratas(10);
+            // Eljárás Rekurzívan, listafeltöltés(db)
+            // FV Rekurzívan, egy összegzés tételét (ciklus változó)!
+
+            RekurzivListaFeltoltes(5);
+            Console.WriteLine("\nA számok összge: "+RekurzivOsszegzes(0));
+
+            Console.ReadLine();
+        }
+
+        static int RekurzivOsszegzes(int n)
+        {
+            if (n < lista.Count)
+            {
+                return lista[n] + RekurzivOsszegzes(n + 1);
+            }
+            return 0;
+        }
+
+        static void RekurzivListaFeltoltes(int n)
+        {
+            if (n > 0)
+            {
+                lista.Add(r.Next(0,5));
+                RekurzivListaFeltoltes(n - 1);
+            }
+        }
+
+        static void Kiiratas(int n)
+        {
+            Console.Write(n + " ");
+            if (n > 1)
+                Kiiratas(n - 1);
+        }
+
+        static int FibonacciRekurzivan(int n)
+        {
+            if (n < 3)
+            {
+                return n;
+            }
+            else
+                return FibonacciRekurzivan(n - 1) + FibonacciRekurzivan(n - 2);
+        }
+
+        static int FibonacciIterativan()
+        {
+            List<int> fib = new List<int> {0,  1 };
+            int n = 10;
+            for (int i = 2; i < n; i++)
+            {
+                fib.Add(fib[i - 1] + fib[i - 2]);
+            }
+            return fib.Last();
+        }
+
+        static int FaktorRekurzivan(int n)
+        {
+            if (n > 0) return n * FaktorRekurzivan(n - 1);
+            else return 1; 
+        }
+
+        static int FaktorIterativan(int n)
+        {
+            int fakt = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                fakt *= i;
+            }
+            return fakt;
+        }
+    }
+}
---
+++ 2024_02_29_RekurzivMatrix/2024_02_29_RekurzivMatrix/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2024_02_29_RekurzivMatrix
+{
+    class Program
+    {
+        static string[,] t = new string[20, 20];
+        static void Main(string[] args)
+        {
+            MatrixFeltoltes();
+            MatrixKiir();
+
+            SzigetekMegszamolasa();
+            Console.WriteLine();
+            MatrixKiir();
+
+            Console.ReadLine();
+        }
+
+        private static void SzigetekMegszamolasa()
+        {
+            int db = 0;
+            for (int i = 0; i < t.GetLength(0); i++)
+            {
+                for (int j = 0; j < t.GetLength(1); j++)
+                {
+                    if (t[i, j] == "X")
+                    {
+                        RekSzigetFelismeres(i, j,db);
+                        db++;
+                    }
+                }
+            }
+            Console.WriteLine(db);
+        }
+
+        private static void RekSzigetFelismeres(int s, int o, int db)
+        {
+            t[s, o] = "" + db;
+            if (s - 1 >= 0 && t[s - 1, o] == "X") RekSzigetFelismeres(s - 1, o,  db);
+            if (o + 1 < t.GetLength(1) && t[s, o+1] == "X") RekSzigetFelismeres(s, o+1, db);
+            if (s + 1 < t.GetLength(0) && t[s+1, o] == "X") RekSzigetFelismeres(s+1, o, db);
+            if (o - 1 >=0 && t[s, o - 1] == "X") RekSzigetFelismeres(s, o - 1, db);
+        }
+
+        private static void MatrixKiir()
+        {
+            for (int i = 0; i < t.GetLength(0); i++)
+            {
+                for (int j = 0; j < t.GetLength(1); j++)
+                {
+                    Console.Write(t[i,j]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void MatrixFeltoltes()
+        {
+            Random r = new Random();
+            for (int i = 0; i < t.GetLength(0); i++)
+            {
+                for (int j = 0; j < t.GetLength(1); j++)
+                {
+                    if (r.Next(10) < 4)
+                        t[i, j] = "X";
+                    else
+                        t[i, j] = ".";
+                }
+            }
+        }
+    }
+}

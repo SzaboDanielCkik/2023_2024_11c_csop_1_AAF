fda16dd408423dffed419daa9913d5b16d21bb0f
Szabó Dániel <szabo.daniel@ckik.loc>
2024-03-11 12:07:48 +0100

    2024_03_11

---
+++ 2024_03_11_Vektorok/2024_03_11_Vektorok/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2024_03_11_Vektorok
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Két oszlop: Origóból kiinduló vektorok végpontjai. (x,y)
+             * racionális számok
+             * Feladat1: Adja meg a vektorok hosszának átlagát! (hossz = Gyök(x^2+y^2) )
+             * Feladat2: Van-e olyan vektor, ami rajta van valamelyik tengelyen? (x=0 vagy y=0)*/
+
+
+
+        }
+    }
+}
